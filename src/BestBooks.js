32cafed4c1a00c132856b45efa4226b8c61854b4
Mathew Torres <torres.mathew@gmail.com>
2022-10-03 15:11:41 -0700

    completes lab 11 frontend

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Carousel } from 'react-bootstrap';
+import axios from 'axios';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -9,6 +12,24 @@ class BestBooks extends React.Component {
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+getBooks = async () => {
+  try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/books`);
+      this.setState({
+        books: response.data
+      })
+
+  } catch (error) {
+    console.log("we have an error", error.message);
+    
+  }
+}
+
+  componentDidMount = () => {
+    this.getBooks();
+  }
+
+
 
   render() {
 
@@ -16,16 +37,41 @@ class BestBooks extends React.Component {
 
     return (
       <>
+        <Container>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
+        <Carousel>
         {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+          this.state.books.map((book, idx) => {
+            return (
+              <Carousel.Item key={idx}>
+                <img
+                  className="d-block w-100"
+                  src={"https://images.unsplash.com/photo-1600189261867-30e5ffe7b8da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"}
+                  alt="First slide"
+                  height="700"
+                />
+                <Carousel.Caption>
+                  <h3>{book.title}</h3>
+                  <p>{book.description}</p>
+                  <p>{book.status}</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            )
+          })
+
+         
         ) : (
-          <h3>No Books Found :(</h3>
+          <p>loading...</p>
         )}
+        </Carousel>
+        </Container>
       </>
     )
-  }
+  } 
 }
+
+
+
+
 
 export default BestBooks;
